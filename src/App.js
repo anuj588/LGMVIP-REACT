8fe16bb673de10412411c563d40f925297055e3c
Anuj sharma <72656672+anuj588@users.noreply.github.com>
2021-10-21 20:44:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Users from "./components/Card";
+import "./App.css";
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    
+    this.state = { users_data: [], loading: false };
+
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState() {
+    this.setState({ loading: true });
+
+    setTimeout(
+      async function () {
+        const response = await fetch("https://reqres.in/api/users?page=1");
+        const jsonresponse = await response.json();
+        console.log(jsonresponse);
+        this.setState({ users_data: jsonresponse["data"], loading: false });
+      }.bind(this),
+      2000
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <nav className="navbar">
+          <div className="navitems">
+            <h2>Welcome to LGMVIP</h2>
+            
+            <button className="fetchbtn" onClick={this.updateState}>
+              Get Users
+            </button>
+          </div>
+        </nav>
+        <div className="userdatacontainer">
+          <Users loading={this.state.loading} users={this.state.users_data} />
+          
+          
+        </div>
+      
+        <div class="container my-50">
+          
+       
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
